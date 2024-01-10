8f80664e4c65ef670ec79a9e29475326e379b8eb
Ken Chan <nok4123456@gmail.com>
2024-01-10 18:06:13 +0800

    feat: updated Home component with new layout and styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    // <div className="absolute top-28 left-0 right-0 flex items-center justify-center text-4xl font-bold text-slate-900">
+    //   Hi My name is Ken
+    // </div>
+    <section className="w-full h-screen relative"></section>
+  );
 }
 
 export default Home;
