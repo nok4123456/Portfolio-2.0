c8790d7a6c7579e4537e49ea8372d315cac5e839
Ken Chan <nok4123456@gmail.com>
2024-01-10 16:31:19 +0800

    feat: implemented React Router with Navbar and page routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { About, Contact, Home, Projects } from "./pages";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>Apps</div>;
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
