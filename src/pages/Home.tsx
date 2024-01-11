a812ff4227decadd9a77ffec5fe44fd6fba1541e
Ken Chan <nok4123456@gmail.com>
2024-01-11 12:51:58 +0800

    feat: integrated @react-three/fiber Canvas and Loader component in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+
 function Home() {
   return (
     // <div className="absolute top-28 left-0 right-0 flex items-center justify-center text-4xl font-bold text-slate-900">
     //   Hi My name is Ken
     // </div>
-    <section className="w-full h-screen relative"></section>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{
+          near: 0.1,
+          far: 1000,
+        }}
+      >
+        <Suspense fallback={<Loader />}>Hello</Suspense>
+      </Canvas>
+    </section>
   );
 }
 
